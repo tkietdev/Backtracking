924b09ea28f0e6b43c7bb9753361eadeb4e2d442
Tuấn Kiệt( 22T1020183) <117705056+TUANKIETPHANHUU@users.noreply.github.com>
2026-06-03 14:38:41 +0700

    comit

--- Quyhoachdong/Lietkeso.cpp
+++ Quyhoachdong/Lietkeso.cpp
@@ -1,40 +1,29 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int n;
-int x[100];
+int x[25];
 
-void in()
+void In()
 {
-    int dem = 0;
-
-    for (int i = 1; i < n; i++)
-    {
-        if (x[i] == 1 && x[i + 1] == 0)
-            dem++;
-    }
-
-    if (dem == 1)
-    {
-        for (int i = 1; i <= n; i++)
-            cout << x[i];
-        cout << endl;
-    }
+    for (int i = 1; i <= n; i++)
+        cout << x[i];
+    cout << endl;
 }
 
 void Try(int i)
 {
     for (int j = 0; j <= 1; j++)
     {
+        if (i > 1 && x[i - 1] == 1 && j == 0)
+            continue;
         x[i] = j;
-
         if (i == n)
-            in();
+            In();
         else
             Try(i + 1);
     }
 }
-
 int main()
 {
     cin >> n;
