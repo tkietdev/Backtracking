3c9605b5f14e840064e7e3f4b20a1f278368bdd6
Tuấn Kiệt( 22T1020183) <117705056+TUANKIETPHANHUU@users.noreply.github.com>
2026-06-03 16:59:59 +0700

    comit

---
+++ Quyhoachdong/haidia.cpp
@@ -0,0 +1,47 @@
+include<stdio.h> int n;
+int g[100];
+int x[100];
+int cnt = 0;
+int sum_trai = 0, sum_phai = 0;
+void InKQ() {
+  cnt++;
+  printf("Cach %d: dia trai: ", cnt);
+  for (int i = 1; i <= n; i++)
+    if (x[i] == 1)
+      printf("%d ", g[i]);
+  printf(", dia phai: ");
+  for (int i = 1; i <= n; i++)
+    if (x[i] == 2)
+      printf("%d ", g[i]);
+
+  printf("\n");
+}
+void Try(int i) {
+  for (int j = 0; j <= 2; j++) {
+    x[i] = j;
+    if (j == 1)
+      sum_trai += g[i];
+    if (j == 2)
+      sum_phai += g[i];
+    if (i == n) {
+      if (sum_trai == sum_phai && sum_trai > 0 && sum_phai > 0) {
+        inkq();
+      }
+    } else {
+      Try(i + 1);
+    }
+    if (j == 1)
+      sum_trai -= g[i];
+    if (j == 2)
+      sum_phai -= g[i];
+  }
+}
+int main() {
+  scanf("%d", &n);
+  for (int i = 1; i <= n; i++) {
+    scanf("%d", &g[i]);
+  }
+  Try(1);
+  printf("Tong so cach: %d\n", cnt);
+  return 0;
+}
